4bde718b65a9a78fe781334c889953b6064036f2
Jan Vandorpe <jan.vandorpe@vdab.be>
2014-11-25 11:59:42 +0100

    basis framework

---
+++ src/MijnProject/Presentation/Footer.php
@@ -0,0 +1,9 @@
+<!--footer-->
+<footer>
+    <div class="container">
+        <p>&copy; Scrumteam TTT natuurbeheer | <a href="mailto:info@scrumteamttt.be">mail ons</a></p>
+    </div>
+</footer>
+<!--einde footer-->
+</body>
+</html>
---
+++ src/MijnProject/Presentation/Welkom.php
@@ -0,0 +1,27 @@
+<!--inhoud-->
+<section class="well">
+    <div class="container">
+        <div class="jumbotron">
+            <img src="img/home.jpg" class="img-thumbnail pull-right hidden-xs" />
+            <h1>Natuurbeheer</h1>
+            <p>welkom op de website van Natuurbeheer Vlaanderen</p>
+         </div>
+    </div>
+</section>
+<section>
+    <div class="container">
+        <div class="row">
+            <article class="col-md-4">
+                <h3>Natuurgebieden</h3>
+                <p>klik op één van onderstaande natuurgebieden</p>
+                <ul>
+                    <li><a href="">het Zwin</a></li>
+                    <li><a href="">De zwalm</a></li>
+                </ul>
+            </article>
+           
+            
+        </div>
+    </div>
+</section>
+<!--einde inhoud-->
